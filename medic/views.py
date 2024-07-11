ea21a7f17d0ba3a7235955aff163984601faeebe
krishchuk <ms.den.best@mail.ru>
2024-07-12 00:17:50 +0500

    start with empty page + register and login

--- medic/views.py
+++ medic/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'medic/home.html'
+    extra_context = {
+            'title': 'MEDIC - Медицинская диагностика'
+        }
